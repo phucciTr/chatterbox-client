3d1063130f71987a6028b87ab99de1bdb3abfbc4
Javier De La Fuente <javierdelafuentejr@gmail.com>
2020-11-17 20:47:16 -0600

    Complete displaying messages from parse server

---
+++ client/scripts/messages.js
@@ -0,0 +1,33 @@
+// {
+//   "messages": [{
+//       "username": "Phucci",
+//       "text": "Hello World!",
+//       "roomname": "JP"
+//     }, {
+//       "username": "Javier",
+//       "text": "Hello World again!",
+//       "roomname": "JP"
+//     }, {
+//       "username": "HackReactor",
+//       "text": "You're an engineer!",
+//       "roomname": "JP"
+//     }]
+// }
+
+
+var data = {
+  "messages": [
+    {
+      username: "Phucci",
+      text: "Hello World!",
+      roomname: "JP"
+    }, {
+      "username": "Javier",
+      "text": "Hello World again!",
+      "roomname": "JP"
+    }, {
+      "username": "HackReactor",
+      "text": "You're an engineer!",
+      "roomname": "JP"
+    }]
+};
